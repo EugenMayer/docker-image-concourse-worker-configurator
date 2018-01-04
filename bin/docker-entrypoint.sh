a1c8a64ae34c09b647035bc1a1639a9dbf1c6d29
em <eugen.mayer@kontextwork.de>
2018-01-04 16:06:18 +0100

    fix private key

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 #set -x
 set -e
 
-echo "${TSA_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/worker_key
+echo "${TSA_PRIVATE_KEY}" > ${WORKER_KEY_HOME}/worker_key
 echo "${TSA_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/tsa_host_key.pub
 
 exec $@
