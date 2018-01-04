f907fb345b867fb4235eac9f50130126e7426e16
em <eugen.mayer@kontextwork.de>
2018-01-04 16:33:46 +0100

    use -e to expand newlines

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 #set -x
 set -e
 
-echo "${TSA_PRIVATE_KEY}" > ${WORKER_KEY_HOME}/worker_key
-echo "${TSA_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/tsa_host_key.pub
+# we need -e to replace the \n in the key, since we pass it using \n most probably
+echo -e "${TSA_PRIVATE_KEY}" > ${WORKER_KEY_HOME}/worker_key
+echo -e "${TSA_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/tsa_host_key.pub
 
 exec $@
