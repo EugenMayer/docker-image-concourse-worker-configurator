7a054590b7851538a4305d55da2f8e3f7d61f8aa
em <eugen.mayer@kontextwork.de>
2018-01-04 19:19:49 +0100

    use better variable names

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # we need -e to replace the \n in the key, since we pass it using \n most probably
-echo -e "${TSA_PRIVATE_KEY}" > ${WORKER_KEY_HOME}/worker_key
-echo -e "${TSA_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/tsa_host_key.pub
+echo -e "${WORKER_EXISTING_PRIVATE_KEY}" > ${WORKER_KEY_HOME}/worker_key
+echo "${TSA_EXISTING_PUBLIC_KEY}" > ${WORKER_KEY_HOME}/tsa_host_key.pub
 
 exec $@
